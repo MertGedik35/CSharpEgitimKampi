cfa981a9a4900a11dd98453bc65c921eba873e96
Mert Gedik <mertgedik9999@gmail.com>
2025-08-10 20:41:50 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba, Dünya!");
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
